457118cf2a5886a479621381a201ae445f5ee66d
ha duy long <longha1032002@gmail.com>
2024-12-03 09:56:47 +0700

    format date, test create company

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "name không được để trống")
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String address;
+
+    private String logo;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+}
